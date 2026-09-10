293be73196cafd31e957a1bbfc21fc2340df195f
Istvan Kardos <kardosistvan98@gmail.com>
2026-09-10 09:58:45 +0200

    Task-3: implement sensorReading saving

--- src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
@@ -1,10 +1,13 @@
 package com.aldisued.iot.monitoring.service;
 
 import com.aldisued.iot.monitoring.dto.SensorReadingDto;
+import com.aldisued.iot.monitoring.entity.Sensor;
 import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
 import com.aldisued.iot.monitoring.repository.SensorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SensorReadingService {
@@ -18,9 +21,13 @@ public class SensorReadingService {
     this.sensorRepository = sensorRepository;
   }
 
+  @Transactional
   public SensorReading saveSensorReading(SensorReadingDto sensorReadingDto) {
-    //TODO: Task 3
-    return null;
+    Sensor sensor = sensorRepository.findById(sensorReadingDto.sensorId())
+            .orElseThrow(() -> new EntityNotFoundException("Sensor not found with ID: " + sensorReadingDto.sensorId()));
+
+    SensorReading sensorReading = new SensorReading(sensorReadingDto.value(), sensorReadingDto.timestamp(), sensor);
+    return sensorReadingRepository.save(sensorReading);
   }
 
 }
