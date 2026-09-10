2d5ae671810ee8922b6ab623cc28db2b9f4ee595
Istvan Kardos <kardosistvan98@gmail.com>
2026-09-10 10:27:21 +0200

    Task-3 follow-up: map EntityNotFoundException to a ProblemDetail 404

--- src/main/java/com/aldisued/iot/monitoring/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/aldisued/iot/monitoring/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.aldisued.iot.monitoring.exception.handler;
 
 import com.aldisued.iot.monitoring.exception.SensorNameAlreadyExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SensorNameAlreadyExistsException.class)
     ProblemDetail handleSensorNameAlreadyExist(SensorNameAlreadyExistsException e) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    ProblemDetail handleEntityNotFound(EntityNotFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
     }
 }
