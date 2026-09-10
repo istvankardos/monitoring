3079b14644dcd548ba1a6e55123e5b16e52b4e02
Istvan Kardos <kardosistvan98@gmail.com>
2026-09-10 11:45:00 +0200

    Task-9: implement filter by average deviation

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -8,8 +8,26 @@ import org.springframework.stereotype.Service;
 public class MeasurementCalculatorService {
 
   public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
+    if (deviation == null || deviation < 0.0 || deviation > 1.0) {
+      throw new IllegalArgumentException("Deviation is must be between 0.0 and 1.0. It was: " + deviation);
+    }
+
+    if (values == null || values.isEmpty()) {
+      return List.of();
+    }
+
+    double average = values.stream()
+            .mapToDouble(Double::doubleValue)
+            .average()
+            .orElseThrow();
+
+    double range = Math.abs(average * deviation);
+    double rangeLower = average - range;
+    double rangeUpper = average + range;
+
+    return values.stream()
+            .filter(value -> value >= rangeLower && value <= rangeUpper)
+            .toList();
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
